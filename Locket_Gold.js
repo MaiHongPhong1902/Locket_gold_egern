05c5faee587a3c69a9c631adb7b800930b04dfa5
mai hong phong <127371458+MaiHongPhong1902@users.noreply.github.com>
2024-12-26 18:34:21 +0700

    Update Locket_Gold.js

--- Locket_Gold.js
+++ Locket_Gold.js
@@ -15,17 +15,17 @@ const ent = JSON.parse(data);
 
 let jsonToUpdate = {
         "request_date_ms": 1704070861000,
-        "request_date": "2022-04-03T01:01:01Z",
+        "request_date": "2002-10-20T01:01:01Z",
         "subscriber": {
             "entitlement": {},
-            "first_seen": "2022-04-03T01:01:01Z",
+            "first_seen": "2002-10-20T01:01:01Z",
             "original_application_version": "1.94.0",
             "last_seen": "2022-04-03T01:01:01Z",
             "other_purchases": {},
             "management_url": null,
             "subscriptions": {},
             "entitlements": {},
-            "original_purchase_date": "2022-04-03T01:01:01Z",
+            "original_purchase_date": "2002-10-20T01:01:01Z",
             "original_app_user_id": "70B24288-83C4-4035-B001-572285B21AE1",
             "non_subscriptions": {}
         }
@@ -40,9 +40,9 @@ for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapp
 
   for (const entitlement of entitlements) {
     jsonToUpdate.subscriber.entitlements[entitlement] = {
-      "purchase_date": "2022-04-03T01:01:01Z",
-      "original_purchase_date": "2022-04-03T01:01:01Z",
-      "expires_date": "9692-01-01T01:01:01Z",
+      "purchase_date": "2002-10-20T01:01:01Z",
+      "original_purchase_date": "2002-10-20T01:01:01Z",
+      "expires_date": "3002-10-20T01:01:01Z",
       "is_sandbox" : false,
       "ownership_type": "PURCHASED",
       "store": "app_store",
@@ -51,9 +51,9 @@ for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapp
 
     // Add product identifier to subscriptions
     jsonToUpdate.subscriber.subscriptions[productIdentifier] = {
-      "expires_date": "9692-01-01T01:01:01Z",
-      "original_purchase_date": "2022-04-03T01:01:01Z",
-      "purchase_date": "2022-04-03T01:01:01Z",
+      "expires_date": "3002-10-20T01:01:01Z",
+      "original_purchase_date": "2002-10-20T01:01:01Z",
+      "purchase_date": "2002-10-20T01:01:01Z",
       "is_sandbox" : false,
       "ownership_type": "PURCHASED",
       "store": "app_store"
